df7c0a7c9009bc1770626ec387ea13d1ec1573be
Tyron Vergara <tyron.vergara@gmail.com>
2022-06-10 23:49:04 -0500

    Progreso en contacto (HTML/CSS y JS)

--- js/contacto.js
+++ js/contacto.js
@@ -10,18 +10,22 @@ let datosEmail = {
     Body : "" //
 } 
 
-function validarNombre(taco){
-    if (taco.length < 3) {
+function validarNombre(nombre){
+    if (nombre.length < 1) {
       return false;
     }
     return true;
   } //validarNombre
 
-  function validarEmail(email) {
-    if (email() ){
-     return false;
-  }
-  return true;
+  function validarEmail(direccion) {
+
+    let direccionValida = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (direccion.match(direccionValida)){
+     return true;
+    }
+
+    return false;
   } //validarEmail
 
   function validarTelefono(telefono){
@@ -32,11 +36,18 @@ function validarNombre(taco){
   } //validarTelefono
   
   function validarMensaje(mensaje){
-    if (mensaje.length >3 && mensaje.length<200 ) {
+    if (mensaje.length <= 0 || mensaje.length > 1000 ) {
       return false;
     }
     return true;
   }//validarMensaje
+
+  function validarAsunto(asunto) {
+    if (asunto.length <= 0 || asunto.length > 1000) {
+      return false;
+    }
+    return true;
+  }//validarAsunto
   
 botonEnviar.addEventListener("click", (event) => {
     
@@ -46,32 +57,53 @@ botonEnviar.addEventListener("click", (event) => {
     let telefonoFormulario = document.getElementById("telefono").value;
     let mensajeFormulario = document.getElementById("mensaje").value;
     let correoFormulario = document.getElementById("correoElectronico").value;
+    let asuntoFormulario = document.getElementById("asunto").value;
+    let listaErrores = "";
 
+    // Enviar de forma temporal el correo al remitente (para comprobar funcionamiento)
     datosEmail.To = correoFormulario
-    // datosEmail.To = document.getElementById("correoElectronico").value;
-    datosEmail.Subject = "Mensaje del formulario de contacto";
+
+    // Definir "asunto" y "cuerpo" del mensaje
+
+    datosEmail.Subject = asuntoFormulario;
     
     datosEmail.Body = "Nombre del cliente: " + nombreFormulario + "<br>" +
                       "Teléfono del cliente: "  + telefonoFormulario  +"<br>" +
                       "Mensaje del cliente: " + mensajeFormulario;
 
-    if (! validarNombre(nombreFormulario) ) {
-        document.getElementById("nombre").value = "El nombre es muy corto";
-        return false;
+    // Validaciones...
+
+    if ( ! validarNombre(nombreFormulario) ) {
+      
+    }
+
+    if (! validarEmail(correoFormulario) ) {
+      
     }
 
     if (! validarTelefono(telefonoFormulario) ) {
-      document.getElementById("telefono").value = "El teléfono es invalido";
-      return false;
-  }
+      
+    }
+
+    if (! validarAsunto(asuntoFormulario) ) {
+      
+    }
+
+    if ( ! validarMensaje(mensajeFormulario) ) {
+      
+    }
 
     Email.send(datosEmail).then(
+        // CAMBIAR - (Alerta solo para pruebas)
         message => alert("Email enviado :)")
-    );
-  
+
+    // Regresando el formulario a datos en blanco
       document.getElementById("correoElectronico").value = "";
       document.getElementById("mensaje").value = "";
       document.getElementById("nombre").value = "";
       document.getElementById("telefono").value = "";
+      document.getElementById("asunto").value = "";
+
+    }
     
 })
