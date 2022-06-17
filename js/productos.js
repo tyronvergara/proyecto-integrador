f1e8a546de9787e72d447a611cb01f2b9a82778b
Tyron Vergara <tyron.vergara@gmail.com>
2022-06-16 23:58:50 -0500

    feat: función para agregar productos al html mediante js
    Se agregó la función al archivo JS necesaria para agregar los objetos dentro del HTML de forma dinámica

--- js/productos.js
+++ js/productos.js
@@ -1,94 +1,137 @@
+let out001 = {"sku":"out001",
+"nombre":
+"Bota Casual Levis para Hombre L211332 Cafe",
+"img":"./img/productos/caballeros/out001/botas1.png",
+"precio":"1599.00",
+"tallas":[25,26,27,28,29,30],
+"existencias":"28",
+"producto":"bota",
+"categoria":"caballero"}
 
+let out002 = {"sku":"out002",
+"nombre":"Tenis Adidas para Hombre FY8568 Blanco",
+"img" : "img/productos/caballeros/out002/sandalia1.png",
+"precio":"1199.00",
+"tallas":[25,26,27,28,29,30],
+"existencias":"15",
+"producto":"tenis",
+"catagoria":"caballeros"};
 
- out001 = {"Sku":"out001","Nombre del Producto":"Bota Casual Levis para Hombre L211332 Cafe",
-    "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/d0da6742-89fd-4d96-99ea-e2a721e13007_600x.png?v=1655290687",
-    "Precio":"1599.00",
-    "Tallas":"25-30",
-    "Existencias":"28",
-    "Tipo de Producto":"Bota",
-    "Catagoria":"Caballero"}
+let out003 = {"sku":"out003",
+"nombre":"Sandalias Nike para Hombre CN9675-002",
+"img": "img/prodectos/caballeros/out003/tenis1.png",
+"precio":"749.00",
+"tallas":[25,26,27,28,29,30],
+"existencias":"30",
+"producto":"sandalias",
+"catagoria":"caballeros"}
 
-out002 = {"Sku":"out002","Nombre del Producto":"Tenis Adidas para Hombre FY8568 Blanco",
-        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/a2ee0ee2-59fb-4245-8b7b-f2700a1d54fa_a7a4b5b8-a2e5-4527-bc60-ccbc11b2771b_600x.png?v=1655294786",
-        "Precio":"1199.00",
-        "Tallas":"25-30",
-        "Existencias":"15",
-        "Tipo de Producto":"Tenis",
-        "Catagoria":"Caballeros"};
+ let out004= {"Sku":"out004",
+ "nombre":"Bota Paulina para Mujer Cafe",
+"precio":"659.00",
+"tallas":[22,23,24,25,26],
+"existencias":"25",
+"tipo de Producto":"Bota",
+"catagoria":"Damas",
+"img":"./img/productos/Damas/out004/4.webp"}
 
-out003 = {"Sku":"Out0033","Nombre del Producto":"Sandalias Nike para Hombre CN9675-002",
-        "img": "https://cdn.shopify.com/s/files/1/0031/3271/9173/products/677510fb-e113-4495-903c-37179671f132_600x.png?v=1655405139",
-        "Precio":"749.00",
-        "Tallas":"25-30",
-        "Existencias":"30",
-        "Tipo de Producto":"Sandalias",
-        "Categoria":"Caballeros"}
+let out005= {"Sku":"outoo5",
+"nombre":"Tenis Adidas para Mujer GY0700 Negro",
+"precio":"1299.00",
+"tallas":[22,23,24,25,26],
+"existencias":"21",
+"producto":"Tenis",
+"catagoria":"Damas",
+"img":"./img/productos/Damas/out005/5.webp"}
 
-    let out004= {"sku":"out004","producto":"Bota Paulina para Mujer Cafe",
-        "precio":"659.00",
-        "tallas":[22,23,24,25,26],
-        "existencias":"25",
-        "tipo":"Bota",
-        "categoria":"Damas","img":"./img/productos/Damas/out004/4.webp"}
+let out006= {"Sku":"outoo6",
+"nombre":"Zapatillas Paulina para Mujer 14215 Cafe",
+"precio":"469.00",
+"tallas":[22,23,24,25,26],
+"existencias":"12",
+"tipo de Producto":"Zapatillas",
+"catagoria":"Damas",
+"img":"./img/productos/Damas/out006/6.webp"}
 
-        let out005= {"sku":"out005","producto":"Tenis Adidas para Mujer GY0700 Negro",
-        "precio":"1299.00",
-        "tallas":[22,23,24,25,26],
-        "existencias":"21",
-        "tipo":"Tenis",
-        "categoria":"Damas","img":"./img/productos/Damas/out005/5.webp"}
+let out007= {"Sku":"outoo7",
+"nombre":"Tenis Adidas para Niña GZ3318 Blanco",
+"precio":"999.00",
+"tallas":[16,17,18,19,20,21],
+"existencias":"13",
+"producto":"Tenis niñas",
+"catagoria":"Niños",
+"img":"./img/productos/Nino/out007/7.webp"}
+
+let out008= {"Sku":"outoo8",
+"nombre":"Zapato Casual Jakuna para Niña 21854 Rojo",
+"precio":"299.00",
+"tallas":[16,17,18,19,20,21],
+"existencias":"6",
+"producto":"Zapato",
+"catagoria":"Niños",
+"img":"./img/productos/Nino/out008/8.webp"}
+
+let out009= {"Sku":"outoo9",
+"nombre":"Tenis Marvel para Niño 73731 Rojo ",
+"precio":"499.00",
+"tallas":[16,17,18,19,20,21],
+"existencias":"14",
+"producto":"Zapato",
+"catagoria":"Niños",
+"img":"./img/productos/Nino/out009/9.webp"}
+
+let out010 = {"sku":"out0010",
+"nombre":"Sandalias Minipapos para Niño 785 Negro",
+"img":"img/productos/bebes/out10/sandaliasbebe1.png",
+"precio":"319.00",
+"tallas":[13,14,15,16],
+"existencias":"30",
+"producto":"sandalias",
+"categoria":"bebes"}
         
-        let out006= {"sku":"out005","producto":"Zapatillas Paulina para Mujer 14215 Cafe",
-        "precio":"469.00",
-        "tallas":[22,23,24,25,26],
-        "existencias":"12",
-        "tipo":"Zapatillas",
-        "categoria":"Damas","img":"./img/productos/Damas/out006/6.webp"}
+let out011= {"sku":"out011",
+"nombre":"Tenis Minipapos para Niño 35306 Azul marino",
+"img":"img/productos/bebes/out11/bebesnino.png",
+"Precio":"1199.00",
+"tallas":[13,14,15,16],
+"existencias":"15",
+"producto":"Tenis",
+"categoria":"bebes"}
+        
+let out012= {"sku":"out013",
+"nombre":"Tenis Nike para Niña AR4137-510 Lila",
+"img":"img/productos/bebes/out12/bebesnina.png",
+"precio":"1049.00.00",
+"tallas":[13,14,15,16],
+"existencias":"10",
+"producto":"tenis",
+"categoria":"bebes"}
 
-        let out007= {"sku":"out007","producto":"Tenis Adidas para Niña GZ3318 Blanco",
-        "precio":"999.00",
-        "tallas":[16,17,18,19,20,21,22],
-        "existencias":"13",
-        "tipo":"Tenis niñas",
-        "categoria":"Niños","img":"./img/productos/Nino/out007/7.webp"}
 
-        let out008= {"sku":"out008","producto":"Zapato Casual Jakuna para Niña 21854 Rojo [JAK155]",
-        "precio":"299.00",
-        "tallas":[16,17,18,19,20,21,22],
-        "existencias":"6",
-        "tipo":"Zapato",
-        "categoria":"Niños","img":"./img/productos/Nino/out008/8.webp"}
+function agregaProducto(producto){
+    const itemHTML =    `<div class="col mb-4">
+                                <div class="card">
+                                        <img id="imgProducto" src="${producto.img}">
+                                        <div class="card-body">
+                                                <h5 class="card-title"> ${producto.nombre} </h5>
+                                                <p  class="card-text"> ${producto.precio} </p>
+                                        </div>
+                                </div>
+                        </div>`
 
-        let out009= {"sku":"out009","producto":"Tenis Marvel para Niño 73731 Rojo ",
-        "precio":"499.00",
-        "tallas":[16,17,18,19,20,21,22],
-        "existencias":"14",
-        "tipo":"Zapato",
-        "categoria":"Niños","img":"./img/productos/Nino/out009/9.webp"}
+    const itemsContainer = document.getElementById("listaProductos");
+    itemsContainer.innerHTML += itemHTML;
+}
 
-        out10 = {"Sku":"out0010","Nombre del Producto":"Sandalias Minipapos para Niño 785 Negro",
-        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/c32d5a03-f4cd-4512-ad78-730cd77e8e1b_600x.png?v=1655361213",
-        "Precio":"319.00",
-                "Tallas":"13-16",
-                "Existencias":"30",
-                "Tipo de Producto":"Sandalias",
-                "Catagoria":"Bebes"}
-                
-    out11= {"Sku":"out011","Nombre del Producto":"Tenis Minipapos para Niño 35306 Azul marino",
-
-        "Precio":"1199.00",
-        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/35ae26a1-8e0c-4caf-96cc-d2156a791add_300x.png?v=1655360851",
-                "Tallas":"13-16",
-                "Existencias":"15",
-                "Tipo de Producto":"Tenis",
-                "Catagoria":"Bebes"}
-                
-    out12= {"Sku":"out013","Nombre del Producto":"Tenis Nike para Niña AR4137-510 Lila",
-             "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/7f6b6e89-db01-4137-a326-8d4b25004a61_600x.png?v=1655039202",
-             "Precio":"1049.00.00",
-             "Tallas":"13-16",
-             "Existencias":"10",
-             "Tipo de Producto":"Tenis",
-             "Catagoria":"Bebes"}
-                       
-
+agregaProducto(out001);
+agregaProducto(out002);
+agregaProducto(out003);
+agregaProducto(out004);
+agregaProducto(out005);
+agregaProducto(out006);
+agregaProducto(out007);
+agregaProducto(out008);
+agregaProducto(out009);
+agregaProducto(out010);
+agregaProducto(out011);
+agregaProducto(out012);
