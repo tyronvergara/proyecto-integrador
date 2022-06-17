837a4a9aafd0e89e4431842baa821990b00c2377
AntonioBonilla <joseantoniojoba@gmail.com>
2022-06-16 19:57:48 -0500

    productos caballeros-bebes

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,28 @@
 
 
+ out001 = {"Sku":"out001","Nombre del Producto":"Bota Casual Levis para Hombre L211332 Cafe",
+    "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/d0da6742-89fd-4d96-99ea-e2a721e13007_600x.png?v=1655290687",
+    "Precio":"1599.00",
+    "Tallas":"25-30",
+    "Existencias":"28",
+    "Tipo de Producto":"Bota",
+    "Catagoria":"Caballero"}
+
+out002 = {"Sku":"out002","Nombre del Producto":"Tenis Adidas para Hombre FY8568 Blanco",
+        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/a2ee0ee2-59fb-4245-8b7b-f2700a1d54fa_a7a4b5b8-a2e5-4527-bc60-ccbc11b2771b_600x.png?v=1655294786",
+        "Precio":"1199.00",
+        "Tallas":"25-30",
+        "Existencias":"15",
+        "Tipo de Producto":"Tenis",
+        "Catagoria":"Caballeros"};
+
+out003 = {"Sku":"Out0033","Nombre del Producto":"Sandalias Nike para Hombre CN9675-002",
+        "img": "https://cdn.shopify.com/s/files/1/0031/3271/9173/products/677510fb-e113-4495-903c-37179671f132_600x.png?v=1655405139",
+        "Precio":"749.00",
+        "Tallas":"25-30",
+        "Existencias":"30",
+        "Tipo de Producto":"Sandalias",
+        "Catagoria":"Caballeros"}
     let out004= {"Sku":"outoo4","Nombre del Producto":"Bota Paulina para Mujer Cafe",
         "Precio":"659.00",
         "Tallas":"22 - 26",
@@ -41,3 +64,30 @@
         "Existencias":"14",
         "Tipo de Producto":"Zapato",
         "Catagoria":"Niños","img":"./img/productos/Nino/out009/9.webp"}
+
+        out10 = {"Sku":"out0010","Nombre del Producto":"Sandalias Minipapos para Niño 785 Negro",
+        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/c32d5a03-f4cd-4512-ad78-730cd77e8e1b_600x.png?v=1655361213",
+        "Precio":"319.00",
+                "Tallas":"13-16",
+                "Existencias":"30",
+                "Tipo de Producto":"Sandalias",
+                "Catagoria":"Bebes"}
+                
+    out11= {"Sku":"out011","Nombre del Producto":"Tenis Minipapos para Niño 35306 Azul marino",
+
+        "Precio":"1199.00",
+        "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/35ae26a1-8e0c-4caf-96cc-d2156a791add_300x.png?v=1655360851",
+                "Tallas":"13-16",
+                "Existencias":"15",
+                "Tipo de Producto":"Tenis",
+                "Catagoria":"Bebes"}
+                
+    out12= {"Sku":"out013","Nombre del Producto":"Tenis Nike para Niña AR4137-510 Lila",
+             "img":"https://cdn.shopify.com/s/files/1/0031/3271/9173/products/7f6b6e89-db01-4137-a326-8d4b25004a61_600x.png?v=1655039202",
+             "Precio":"1049.00.00",
+             "Tallas":"13-16",
+             "Existencias":"10",
+             "Tipo de Producto":"Tenis",
+             "Catagoria":"Bebes"}
+                       
+
