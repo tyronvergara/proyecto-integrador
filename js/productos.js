04e58c1c0904a315bd6d76141f6c04dda1a72b32
Gabriel malacara <jgabrielmr@gmail.com>
2022-06-16 19:25:47 -0500

    Se realizan los objetos con los productos en java

---
+++ js/productos.js
@@ -0,0 +1,43 @@
+
+
+    let out004= {"Sku":"outoo4","Nombre del Producto":"Bota Paulina para Mujer Cafe",
+        "Precio":"659.00",
+        "Tallas":"22 - 26",
+        "Existencias":"25",
+        "Tipo de Producto":"Bota",
+        "Catagoria":"Damas","img":"./img/productos/Damas/out004/4.webp"}
+
+        let out005= {"Sku":"outoo5","Nombre del Producto":"Tenis Adidas para Mujer GY0700 Negro",
+        "Precio":"1299.00",
+        "Tallas":"22 - 26",
+        "Existencias":"21",
+        "Tipo de Producto":"Tenis",
+        "Catagoria":"Damas","img":"./img/productos/Damas/out005/5.webp"}
+        
+        let out006= {"Sku":"outoo5","Nombre del Producto":"Zapatillas Paulina para Mujer 14215 Cafe",
+        "Precio":"469.00",
+        "Tallas":"22 - 26",
+        "Existencias":"12",
+        "Tipo de Producto":"Zapatillas",
+        "Catagoria":"Damas","img":"./img/productos/Damas/out006/6.webp"}
+
+        let out007= {"Sku":"outoo7","Nombre del Producto":"Tenis Adidas para Niña GZ3318 Blanco",
+        "Precio":"999.00",
+        "Tallas":"16 - 21",
+        "Existencias":"13",
+        "Tipo de Producto":"Tenis niñas",
+        "Catagoria":"Niños","img":"./img/productos/Nino/out007/7.webp"}
+
+        let out008= {"Sku":"outoo8","Nombre del Producto":"Zapato Casual Jakuna para Niña 21854 Rojo [JAK155]",
+        "Precio":"299.00",
+        "Tallas":"16 - 21",
+        "Existencias":"6",
+        "Tipo de Producto":"Zapato",
+        "Catagoria":"Niños","img":"./img/productos/Nino/out008/8.webp"}
+
+        let out009= {"Sku":"outoo9","Nombre del Producto":"Tenis Marvel para Niño 73731 Rojo ",
+        "Precio":"499.00",
+        "Tallas":"16 - 21",
+        "Existencias":"14",
+        "Tipo de Producto":"Zapato",
+        "Catagoria":"Niños","img":"./img/productos/Nino/out009/9.webp"}
